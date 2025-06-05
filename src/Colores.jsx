216e251eb9361c29779a450465b5a3953dfe72fb
yoanna <yoannarrd@gmail.com>
2025-06-05 14:43:51 +0200

    links a render

--- src/Colores.jsx
+++ src/Colores.jsx
@@ -9,7 +9,7 @@ export default function Colores(){
     // carga inicial de los datos
     //useEffect es como hacer la conexión inicial a la db para traer los colores a react
     useEffect( () => { 
-        fetch("http://localhost:3000/colores")
+        fetch("https://colores-react-uytr.onrender.com/colores")
         .then(respuesta => respuesta.json())
         .then( colores => setColores(colores) )
     }, [])
